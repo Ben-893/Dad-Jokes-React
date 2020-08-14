0693d066b73d3270ef99a1fd53df3b54ca2d1041
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-08-14 18:15:53 +0100

    Changed Audio file and some CSS

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <div className="mx-auto">
-      <h2>This is a small React Application for generating random dad jokes :)</h2>
+      <h1>This is a small React Application for generating random dad jokes :)</h1>
     </div>
   );
 }
